5f2a7e644fc0d0660c8f449920875353ff763008
Brendan Russo <brendanrusso27@gmail.com>
2016-09-07 16:37:21 +1000

    - Added first function to BobsHelper - BNull.AreNull(params object[]) checks if a list of parameters are null and will return true or false

---
+++ BobsHelper.Tests/BNull_UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BobsHelper;
+
+namespace BobsHelper.Tests
+{
+    [TestClass]
+    public class BNull_UnitTest
+    {
+        [TestMethod]
+        public void BNull_AreNull_True()
+        {
+            int? nullInt = null;
+            object nullObject = null;
+            string nullString = null;
+            Assert.IsTrue(BNull.AreNull(nullInt, nullObject, nullString));
+        }
+
+        public void BNull_AreNull_False()
+        {
+            int? nullInt = null;
+            object nullObject = null;
+            string nullString = "";
+
+            Assert.IsFalse(BNull.AreNull(nullInt, nullObject, nullString));
+        }
+    }
+}
---
+++ BobsHelper/BNull.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobsHelper
+{
+    public static class BNull
+    {
+        public static bool AreNull(params object[] nullValues)
+        {
+            //throw new NotImplementedException();
+            var valid = true;
+            foreach(var nullValue in nullValues)
+            {
+                if(nullValue != null)
+                {
+                    valid = false;
+                    break;
+                }
+            }
+            return valid;
+        }
+    }
+}
